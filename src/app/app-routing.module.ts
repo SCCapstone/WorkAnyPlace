16eb7fd0fb81818327392c813250d38a0fcfc3ba
Brandon Race <barace@email.sc.edu>
2021-02-24 14:49:49 -0500

    Recomitting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,6 +40,10 @@ const routes: Routes = [
   {
     path: 'settings',
     loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+  },
+  {
+    path: 'reset-password',
+    loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/reset-password/reset-password.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.page.html',
+  styleUrls: ['./reset-password.page.scss'],
+})
+export class ResetPasswordPage implements OnInit {
+
+  new_product_form: FormGroup;
+  db = firebase.firestore();
+
+  constructor(public afAuth: AngularFireAuth, private router: Router, public formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    var auth = firebase.auth();
+    this.new_product_form = this.formBuilder.group({
+      email: new FormControl('', Validators.required),
+      });
+  }
+
+}
