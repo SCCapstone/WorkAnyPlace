0c76d4ae073a2885094db1ae7a5c1bd54b6d210d
Caleb Ulch <culch@email.sc.edu>
2021-04-25 14:47:42 -0400

    testing

--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -281,6 +281,7 @@ export class JobsService {
     this.db.collection("pendingCompletion").doc(this.user.uid+this.jobToPost.title)
     .onSnapshot((doc) => {
       console.log("Trying to complete");
+      console.log(doc.data());
       if(doc.data().attempt == true) {
         alert("HEy is this job really complete?");
       } else {
